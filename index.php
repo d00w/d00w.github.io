a31b033f5fa24316dcb2e06db8c893ca3b26d7fc
EAt0p <79407205+EAt0p@users.noreply.github.com>
2021-11-12 14:29:32 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+  <title>Home</title>
+  <link rel="stylesheet" href="style.css">
+	<script src="node.js"></script>
+ </head>
+<body>
+  <a id="sbar">&equiv</a>
+  <div id="links">
+    <a id="Home" class="link">Home</a>
+    <a class="link">Downloads</a>
+    <a class="link">About</a>
+  </div>
+</body>
