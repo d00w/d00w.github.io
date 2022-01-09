e54a327ae41fb9a383cbed66b029e9bcf16b14b2
EAt0p <79407205+EAt0p@users.noreply.github.com>
2022-01-09 19:08:55 +0200

    Update node.js

--- node.js
+++ node.js
@@ -1,54 +1,18 @@
-window.onload = function(){
-  var link = document.getElementsByClassName("link");
-  sbar.addEventListener("click", contswtch);
-  link[0].addEventListener("click", hc);
-  link[1].addEventListener("click", dc);
-  link[2].addEventListener("click", ac);
-}
-function contswtch(){
-	if(links.style.width == "100px"){
-	links.style.width = "0px";
-	sbar.style.left="0px";
-	sbar.style.top="0px";
-	sbar.style.transform="rotate(0deg)";
-	}
-	else{
-	links.style.width = "100px";
-	sbar.style.left="111px";
-	sbar.style.top="11px";
-	sbar.style.transform="rotate(90deg)";
-	}
-}
-var contbox = document.getElementsByClassName("contbox");
-function clr(){
-	for(x=0; x<3; x++){
-	document.getElementsByClassName("contbox")[x].style.display = "none";
-	}
+window.onload=function(){
+function saveToFirebase(email) {
+    var emailObject = {
+        email: email
+    };
+
+    firebase.database().ref('subscription-entries').push().set(emailObject)
+        .then(function(snapshot) {
+            success(); // some success method
+        }, function(error) {
+            console.log('error' + error);
+            error(); // some error method
+        });
 }
 
+saveToFirebase("eee@mail.com");
 
-
-function hc(){
-	contswtch();
-	clr();
-	contbox[0].style.display = "block";
-}
-
-
-
-
-function dc(){
-	contswtch();
-	clr();
-	contbox[1].style.display = "block";
-}
-
-
-
-
-
-function ac(){
-	contswtch();
-	clr();
-	contbox[2].style.display = "block";
 }
