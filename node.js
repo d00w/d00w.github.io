0d41a8ca1a5631c6f989d1f13c9e0c0c9aa2b348
EAt0p <doowew100@gmail.com>
2022-02-13 16:47:54 +0000

    updated node.js

--- node.js
+++ node.js
@@ -3,13 +3,4 @@ window.onload=function(){
 	cv.width=window.innerWidth;
 	cv.height=window.innerHeight;
 	var ctx=cv.getContext("2d");
-	var grd=ctx.createLinearGradient(0, 0, cv.width, cv.height);
-	grd.addColorStop(0, "#ff0000");
-	grd.addColorStop(0.33, "#ffff00");
-	grd.addColorStop(0.6, "#00ff00");
-	grd.addColorStop(1, "#0000ff");
-	ctx.beginPath();
-	ctx.fillStyle=grd;
-	ctx.rect(0, 0, cv.width, cv.height);
-	ctx.fill();
 }
